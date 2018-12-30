713c17b4c7e007d5ef14f6f74a9f832e135d7770
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-30 13:46:28 -0800

    Import json web token

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
