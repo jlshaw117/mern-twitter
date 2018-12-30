23b1555a472db1252c609705b71fa10e2d418c29
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-29 18:15:15 -0800

    Add a sucess message to indicate that the server is running

--- app.js
+++ app.js
@@ -4,3 +4,5 @@ const app = express();
 app.get("/", (req, res) => res.send("Hello World"));
 
 const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
