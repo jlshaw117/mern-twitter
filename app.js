8b0cf7c626bbf264b7d119adc676f6e8ee07b6d6
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-29 18:22:07 -0800

    Import mongo db key

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
+const db = require('./config/keys').mongoURI;
 
 app.get("/", (req, res) => res.send("Hello World"));
 
