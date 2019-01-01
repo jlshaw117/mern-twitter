ae92645f4ff3bbe26fa08d512d7f57c231bbe76a
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-31 16:04:39 -0800

    Import passport

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
