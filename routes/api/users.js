0a118f2c71e515c2ba339082459645a80455707e
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-31 16:16:10 -0800

    Refactor to return curretn user

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,7 +10,11 @@ const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
 
 router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
-    res.json({ msg: 'Success' });
+    res.json({
+        id: req.user.id,
+        handle: req.user.handle,
+        email: req.user.email
+    });
 })
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
