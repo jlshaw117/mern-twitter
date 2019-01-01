1f1c78f225f491cd3d4246ffc06607d7ae5b0580
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-01 13:14:12 -0800

    Add protected route to post a tweet

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -31,4 +31,22 @@ router.get('/:id', (req, res) => {
         );
 });
 
+router.post('/',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const { errors, isValid } = validateTweetInput(req.body);
+
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
+
+        const newTweet = new Tweet({
+            text: req.body.text,
+            user: req.user.id
+        });
+
+        newTweet.save().then(tweet => res.json(tweet));
+    }
+);
+
 module.exports = router;
